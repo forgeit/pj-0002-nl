2f438aedb7141cb89cf3ce354ffd3cafc357f292
Kelvin Alves <kelvinpalves@gmail.com>
2017-05-25 14:18:33 -0300

    Novo método para verificar se tem email pendente.

--- Controller/EnvioController.php
+++ Controller/EnvioController.php
@@ -34,8 +34,8 @@ class EnvioController extends Controller {
         require_once './Model/Cliente.php';
         $filaEnvioService = new FilaEnvioService();
         $filaEnvioList = $filaEnvioService->buscarTodosAguardando();
-        print 'teste';
-        print_r($filaEnvioList);
+        // print 'teste';
+        // print_r($filaEnvioList);
         print count($filaEnvioList) > 0;
     }
 
